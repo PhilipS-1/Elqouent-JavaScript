8a11bcb9e4136643b772f693c772028b8088e127
Philip <philisandmann@gmail.com>
2023-08-03 01:32:44 +0200

    add chapter 04

---
+++ chapter04.js
@@ -0,0 +1,83 @@
+//arrays
+const arr = ["bla", 2]
+arr.push(null)
+arr.pop()
+console.log(arr)
+
+//objects
+let day = {
+    squirrel: false,
+    events: ["work", "touched grass", "pizza", "running"]
+};
+day.bla = "test"
+
+console.log("day:", day)
+
+let journal = [];
+
+function addEntry(events, squirrel){
+    journal.push({events, squirrel})
+}
+
+addEntry(["a", "b", "c"], "bla")
+addEntry("test", true)
+
+console.log(journal)
+
+//for each loop
+for (let entry of journal){
+    console.log(`${entry.events.length} events.`);
+}
+
+
+//functions with infinite arguments
+function max(...numbers){
+    let result = -Infinity
+    for(let number of numbers){
+        if(number > result) result = number;
+    }
+    return result;
+}
+
+console.log(max(1,5,123,-2))
+
+//spread out arguments with ... 
+let ns = [1,4,5,2,1]
+console.log(max(...ns))
+
+//destructuring arrays
+function reverseTriple([a, b, c]){
+    return [c,b,a]
+}
+
+console.log(reverseTriple([1,2,3]))
+
+//JSON
+let message = {
+    createdOn: "01.01.1992",
+    text: "bla"
+}
+
+let jsonMessage = JSON.stringify(message)
+//stringify obj. to JSON
+console.log(jsonMessage)
+//parse JSON to obj:
+console.log(JSON.parse(jsonMessage).text)
+
+//exercises
+function range(start, end, step = 1){
+    let result = [];
+    while(start != end){
+        let i = start
+        result.push(i)
+        start = start += step
+    }
+    result.push(end)
+    return result;
+}
+
+function sum(numbers){
+    return numbers.reduce((acc, x) => acc + x, 0)
+}
+
+//reverse array
